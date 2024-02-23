4fabd7b565240d7182fd7bc0d2132fae60a03b4e
Higor Silva <contato.higordeveloper@gmail.com>
2024-02-23 16:22:16 -0300

    add popup and meta tags

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+// BARRA DO CTA (ENTRADA E SAÍDA)
 window.addEventListener("scroll", () => {
   const ctaBar = document.querySelector("#ctaBar")
   const heroSection = document.querySelector(".hero")
@@ -8,4 +9,29 @@ window.addEventListener("scroll", () => {
   } else {
     ctaBar.classList.remove("show")
   }
+})
+
+// POPUP PORTFÓLIO
+const popup = document.querySelector(".popup-wrapper")
+const pageWrapper = document.querySelector(".popup-page")
+const pageImage = document.querySelector("#page")
+const ctaBar = document.querySelector("#ctaBar")
+function handleOpenPopup(index) {
+  const urlPage = [
+    "./src/images/lp-paulo-full.webp",
+    "./src/images/lp-isabella-full.webp",
+    "./src/images/lp-carolina-full.webp",
+    "./src/images/lp-andre-full.webp",
+  ]
+  pageImage.src = urlPage[index]
+  popup.style.display = "flex"
+  ctaBar.classList.remove("show")
+}
+function handleClosePopup() {
+  const ctaBar = document.querySelector("#ctaBar")
+  popup.style.display = "none"
+  ctaBar.classList.add("show")
+}
+pageWrapper.addEventListener("click", (event) => {
+  event.stopPropagation()
 })
