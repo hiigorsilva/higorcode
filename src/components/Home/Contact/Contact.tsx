138bca923025658985d7db7999b26f810a4ee0a1
hiigorsilva <higorscontato@gmail.com>
2025-06-18 19:18:03 -0300

    feat: add contact section to home page

---
+++ src/components/Home/Contact/Contact.tsx
@@ -0,0 +1,65 @@
+import { ContainerLayout } from "@/components/Globals/ContainerLayout";
+import { InstagramIcon } from "@/components/Home/icons/Instagram";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { contacts } from "@/data/home/contact";
+import { Users2Icon } from "lucide-react";
+
+export const ContactSection = () => {
+	return (
+		<section id="experience" className="w-full h-fit py-36">
+			<ContainerLayout className="flex flex-col gap-6">
+				{/* TITLE */}
+				<div className="flex items-center gap-3">
+					<Users2Icon className="size-8 shrink-0 text-orange-300" />
+					<h2 className="font-light text-4xl text-foreground uppercase tracking-tight">
+						Contatos
+					</h2>
+				</div>
+
+				<div className="grid grid-cols-2 items-start gap-16">
+					{/* SUBTITLE */}
+					<div>
+						<p className="max-w-lg w-full text-lg text-muted-foreground">
+							Interessado em trabalhar juntos? Tem uma proposta de trabalho ou
+							projeto? Ficarei feliz em conversar com você!
+						</p>
+					</div>
+
+					{/* CARD CONTACT */}
+					<Card className="max-w-80 w-full">
+						<CardContent className="flex flex-col gap-4">
+							{contacts.map((contact) => (
+								<Button
+									key={contact.title}
+									variant="ghost"
+									className="w-full h-fit justify-start text-start"
+									asChild
+								>
+									<a
+										href={contact.link}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<div className="grid place-content-center size-8 shrink-0 rounded-lg bg-gradient-to-r from-orange-300/15 to-orange-400/15">
+											{contact.icon}
+										</div>
+
+										<div className="flex flex-col gap-0.5">
+											<h4 className="font-normal text-sm text-foreground uppercase leading-none">
+												{contact.title}
+											</h4>
+											<strong className="font-light text-xs text-muted-foreground tracking-tight leading-none">
+												{contact.value}
+											</strong>
+										</div>
+									</a>
+								</Button>
+							))}
+						</CardContent>
+					</Card>
+				</div>
+			</ContainerLayout>
+		</section>
+	);
+};
---
+++ src/components/Home/icons/Email.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import type { ComponentProps } from "react";
+
+type EmailIconProps = ComponentProps<"svg">;
+
+export const EmailIcon = ({ className }: EmailIconProps) => {
+	return (
+		<svg
+			className={cn("size-5 shrink-0 text-orange-300", className)}
+			viewBox="0 0 20 21"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<title>Email</title>
+			<path
+				d="M17.5 4.25H2.5C2.33424 4.25 2.17527 4.31585 2.05806 4.43306C1.94085 4.55027 1.875 4.70924 1.875 4.875V15.5C1.875 15.8315 2.0067 16.1495 2.24112 16.3839C2.47554 16.6183 2.79348 16.75 3.125 16.75H16.875C17.2065 16.75 17.5245 16.6183 17.7589 16.3839C17.9933 16.1495 18.125 15.8315 18.125 15.5V4.875C18.125 4.70924 18.0592 4.55027 17.9419 4.43306C17.8247 4.31585 17.6658 4.25 17.5 4.25ZM15.893 5.5L10 10.9023L4.10703 5.5H15.893ZM16.875 15.5H3.125V6.29609L9.57734 12.2109C9.69265 12.3168 9.84348 12.3755 10 12.3755C10.1565 12.3755 10.3074 12.3168 10.4227 12.2109L16.875 6.29609V15.5Z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
