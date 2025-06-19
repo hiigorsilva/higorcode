d66da39bbbae9642143447ed06a8d4ee20f78cee
hiigorsilva <higorscontato@gmail.com>
2025-06-19 14:14:25 -0300

    style: add methods section responsive

--- src/components/Home/Methods/Methods.tsx
+++ src/components/Home/Methods/Methods.tsx
@@ -4,14 +4,14 @@ import { GemIcon, LanguagesIcon } from "lucide-react";
 
 export const MethodsSection = () => {
 	return (
-		<section id="experience" className="w-full h-fit py-36">
+		<section id="experience" className="w-full h-fit px-5 py-36">
 			<ContainerLayout className="flex justify-center items-center">
-				<div className="w-full grid grid-cols-2 gap-16">
+				<div className="w-full grid grid-cols-1 md:grid-cols-2 gap-16">
 					{/* METHODS */}
 					<div className="flex flex-col gap-8">
 						<div className="flex items-center gap-3">
 							<GemIcon className="size-8 shrink-0 text-orange-300" />
-							<h2 className="font-light text-4xl text-foreground uppercase tracking-tight">
+							<h2 className="font-light text-3xl md:text-4xl text-foreground uppercase tracking-tight">
 								Métodos Ágeis
 							</h2>
 						</div>
@@ -21,7 +21,7 @@ export const MethodsSection = () => {
 							{methods.map((method) => (
 								<li
 									key={method.title}
-									className="w-fit h-fit flex items-center font-light text-base text-orange-300 uppercase leading-none bg-gradient-to-r from-orange-300/15 to-orange-400/15 py-2.5 px-8 rounded-full"
+									className="w-fit h-fit flex items-center font-light text-base text-orange-300 uppercase leading-none bg-gradient-to-r from-orange-300/15 to-orange-400/15 py-2.5 px-4 rounded-full"
 								>
 									{method.title}
 								</li>
@@ -33,7 +33,7 @@ export const MethodsSection = () => {
 					<div className="flex flex-col gap-8">
 						<div className="flex items-center gap-3">
 							<LanguagesIcon className="size-8 shrink-0 text-orange-300" />
-							<h2 className="font-light text-4xl text-foreground uppercase tracking-tight">
+							<h2 className="font-light text-3xl md:text-4xl text-foreground uppercase tracking-tight">
 								Idiomas
 							</h2>
 						</div>
@@ -45,14 +45,14 @@ export const MethodsSection = () => {
 									key={language.title}
 									className="flex justify-start items-center gap-3"
 								>
-									<div className="relative size-2 shrink-0 rounded-full bg-orange-400 border-2 border-orange-300" />
+									<div className="hidden md:block relative size-2 shrink-0 rounded-full bg-orange-400 border-2 border-orange-300" />
 
-									<div className="flex justify-start items-center gap-3">
-										<h3 className="min-w-52 w-fit font-light text-lg uppercase">
+									<div className=" flex flex-wrap justify-start items-center gap-3">
+										<h3 className="w-fit font-light text-lg text-nowrap uppercase">
 											{language.title}
 										</h3>
 
-										<span className="w-fit h-fit flex items-center font-light text-base text-orange-300 text-nowrap uppercase leading-none bg-gradient-to-r from-orange-300/15 to-orange-400/15 py-2.5 px-8 rounded-full">
+										<span className="w-fit h-fit flex items-center font-light text-base text-orange-300 text-nowrap uppercase leading-none bg-gradient-to-r from-orange-300/15 to-orange-400/15 py-2.5 px-4 rounded-full">
 											{language.level}
 										</span>
 									</div>
