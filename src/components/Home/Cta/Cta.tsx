4ba253355905a400efd308114410fae6ce1ef40a
hiigorsilva <higorscontato@gmail.com>
2025-06-19 13:52:37 -0300

    feat: add link to cta button

--- src/components/Home/Cta/Cta.tsx
+++ src/components/Home/Cta/Cta.tsx
@@ -1,17 +1,21 @@
 import { ContainerLayout } from "@/components/Globals/ContainerLayout";
 import { WhatsAppIcon } from "@/components/Globals/WhatsAppIcon";
 import { Button } from "@/components/ui/button";
-import { contact } from "@/data/home/contact";
-import { LightbulbIcon } from "lucide-react";
+import { contacts } from "@/data/home/contact";
+import { StarsIcon } from "lucide-react";
 
 export const CtaSection = () => {
+	const whatsAppLink = contacts.find(
+		(contact) => contact.title === "WhatsApp",
+	)?.link;
+
 	return (
 		<section className="w-full h-fit flex justify-center items-center px-5">
 			<ContainerLayout className="flex flex-col gap-8 py-20 px-5 bg-foreground rounded-2xl">
 				<h2 className="font-light text-4xl text-center text-background uppercase tracking-tight">
 					Vamos conversar?
 				</h2>
-				<div className="max-w-4xl w-full grid grid-cols-2 items-end gap-16 mx-auto">
+				<div className="max-w-4xl w-full grid grid-cols-2 items-start gap-16 mx-auto">
 					{/* TITLE */}
 					<div className="flex flex-col gap-3">
 						<p className="text-lg text-background/75">
@@ -26,14 +30,14 @@ export const CtaSection = () => {
 							className="w-full min-h-10 h-fit text-black bg-gradient-to-r from-orange-300 to-orange-400 hover:from-orange-400 hover:to-orange-300 hover:scale-[98%] shadow shadow-black/25 hover:shadow-none"
 							asChild
 						>
-							<a href="http://" target="_blank" rel="noopener noreferrer">
+							<a href={whatsAppLink} target="_blank" rel="noopener noreferrer">
 								<WhatsAppIcon />
 								Entrar em contato
 							</a>
 						</Button>
 
 						<span className="flex justify-start items-center gap-1.5 text-sm text-background/75">
-							<LightbulbIcon className="size-4 shrink-0 text-background/75" />
+							<StarsIcon className="size-4 shrink-0 text-background/75" />
 							Respondo rapidamente
 						</span>
 					</div>
