2e8000ac7387ea57cdd044eb54f0a634ce8e291e
Higor Code <higorscontato@gmail.com>
2025-08-23 20:53:46 -0300

    fix: change experience and education copy

--- src/data/home/experience.ts
+++ src/data/home/experience.ts
@@ -4,7 +4,7 @@ export const experiences = [
 		company: "Freelancer",
 		period: "2024 - Atual",
 		description:
-			"Crio Landing Pages otimizadas para aumentar as conversões de vendas de infoprodutos.",
+			"Entreguei Landing Pages com alto poder de conversão para infoprodutores com tempo de carregamento de até 1 segundo para melhor experiência do usuário",
 		techs: [
 			{ name: "TypeScript", icon: "/icons/techs/typescript.svg" },
 			{ name: "Astro.JS", icon: "/icons/techs/astro.svg" },
@@ -19,13 +19,13 @@ export const educations = [
 		company: "B7Web",
 		period: "2023 - Atual",
 		description:
-			"Curso focando em desenvolvimento web Frontend e Backend, que vão desde o absoluto zero até o avançado na programação.",
+			"Aprendi e pratiquei desenvolvimento web com as tecnologias de Frontend e Backend como JavaScript, TypeScript, React, Next, Node.js, Express, PostgreSQL com Prisma, deploy, e outras.",
 	},
 	{
 		title: "Bacharel em Administração",
 		company: "UFG",
 		period: "2019 - 2023",
 		description:
-			"Foco em gestão no setor público e privado, além de desenvolver competências para o planejamento, organização, direção e controle de organizações.",
+			"Desenvolvi conhecimento e aprendizados voltado à gestão pública e privada, com desenvolvimento de competências em planejamento, organização, direção e controle. Para o TCC, elaborei um plano de marketing para uma empresa do setor de construção civil.",
 	},
 ];
